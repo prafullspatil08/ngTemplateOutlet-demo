efe97ecb330b92d4ab324bba2adcd33e25643b7d
prafullpatil <prafull.patil@mindbowser.com>
2023-12-25 18:29:48 +0530

    Feat: generated parent and child components.

--- src/app/modules/example/example.module.ts
+++ src/app/modules/example/example.module.ts
@@ -5,13 +5,17 @@ import { ExampleRoutingModule } from './example-routing.module';
 import { ExampleComponent } from './example.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { PassingDataComponent } from './passing-data/passing-data.component';
+import { ParentComponent } from './parent-child-communication/parent/parent.component';
+import { ChildComponent } from './parent-child-communication/child/child.component';
 
 
 @NgModule({
   declarations: [
     ExampleComponent,
     HomePageComponent,
-    PassingDataComponent
+    PassingDataComponent,
+    ParentComponent,
+    ChildComponent
   ],
   imports: [
     CommonModule,
